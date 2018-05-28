436e91aef61078cf58c204be05ff70333817d9f9
claireb25 <claire.bourgeois.cb@gmail.com>
2018-05-28 17:27:47 +0200

    test

--- controllers/ctrlHome.php
+++ controllers/ctrlHome.php
@@ -1,3 +1,13 @@
 <?php
-echo "test";
-require('views/template.html');
+require('models/model.php');
+
+$test = getFile();
+
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader, array(
+    'cache'=> false
+));
+$template = $twig->load('homepage.html.twig');
+echo $template->render(array('test'=>$test));
--- models/model.php
+++ models/model.php
@@ -1,21 +1,23 @@
-<?
-require_once '../utils/db.php';
+<?php
+require_once 'dbManager.php';
 
 // insert a user
-function insertUser($sender_email,$receiver_email,$message){
-    $this->_db = new dbManager();
-    $response = $db->prepare("INSERT INTO user(sender_email, receiver_email, `message`) VALUES (':sender_email', ':receiver_email',':message')");
-    $response->bindParam(":sender_email", $sender_email);
-    $response->bindParam(":receiver_email", $receiver_email);
-    $response->bindParam(":message", $message);
-    $response->execute();
-    return $db->lastInsertedId();
-}
+// function insertUser($sender_email,$receiver_email,$message){
+    $db = new dbManager("share_files");
+//     echo $db->getBdd();
+	
+//     $response = $db->prepare("INSERT INTO user(sender_email, receiver_email, `message`) VALUES (':sender_email', ':receiver_email',':message')");
+//     $response->bindParam(":sender_email", $sender_email);
+//     $response->bindParam(":receiver_email", $receiver_email);
+//     $response->bindParam(":message", $message);
+//     $response->execute();
+//     return $db->lastInsertedId();
+// }
 
-//insert a file
+// //insert a file
 function insertFile($file_name, $file_size, $id_user){
-    $this->_db = new dbManager();
-    $response = $db->prepare("INSERT INTO `files`(`file_name`, `file_size`, `upload_date`, `delete_date`, id_user) VALUES ('file_name', ':file_size', CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 48 HOUR), ':id_user')");
+   $db = new dbManager("share_files");
+    $response = $db->getBdd()->prepare("INSERT INTO `files`(`file_name`, `file_size`, `upload_date`, `delete_date`, id_user) VALUES ('file_name', ':file_size', CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 48 HOUR), ':id_user')");
     $response->bindParam(":file_name",$file_name,PDO::PARAM_STR);
     $response->bindParam(":file_size",$file_size, PDO::PARAM_INT);
     $response->bindParam(":id_user",$id_user, PDO::PARAM_INT);
@@ -23,9 +25,9 @@ function insertFile($file_name, $file_size, $id_user){
     
 };
 //get a file and all its infos
-function getFile(){
-    $this->_db = new dbManager();
-    $response = $db->prepare("SELECT sender_email,receiver_email,`message`,`file_name`,file_size,delete_date FROM user INNER JOIN files ON user.id = id_user WHERE user.id = :id_user");
+function getFile($id_user){
+    $db = new dbManager("share_files");
+    $response = $db->getBdd()->prepare("SELECT sender_email,receiver_email,`message`,`file_name`,file_size,delete_date FROM user INNER JOIN files ON user.id = id_user WHERE user.id = :id_user");
     $response->bindParam(":id_user", $id_user, PDO::PARAM_INT);
     $response->execute();
     return $response->fetch(PDO::FETCH_ASSOC);
@@ -33,7 +35,7 @@ function getFile(){
 
 
 // delete from bdd after 2 days
-function deleteFile(){
-    $this->_db = new dbManager();
-    $resposne = $db->prepare('DELETE user, files FROM user INNER JOIN files ON user.id = files.id_user WHERE delete_date < NOW()');
-}
+// function deleteFile(){
+//     $this->_db = new dbManager();
+//     $resposne = $db->prepare('DELETE user, files FROM user INNER JOIN files ON user.id = files.id_user WHERE delete_date < NOW()');
+// }
