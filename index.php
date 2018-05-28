fdcced72822f36ab4dad8a91918e75ff30671bec
claireb25 <claire.bourgeois.cb@gmail.com>
2018-05-28 16:22:12 +0200

    dlt utils

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-echo "coucou";
+
 echo $_GET['action'];
 if (isset($_GET['action'])){
     switch ($_GET['action']) { 
         case "home":
             require('controllers/ctrlHome.php');
+        break;
+   
+        case "upload":
+            require('ctrlUpload.php');
         break;
     }
 } else {
--- utils/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=share_files;charset=utf8', 'admin', 'online2017');
-}
-catch(Exception $e) {
-    die('Erreur:'.$e->getMessage());
-}
