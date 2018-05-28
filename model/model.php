be0a24047af3bdcf9a80c6ffbcd5cd1a12384bf0
claireb25 <claire.bourgeois.cb@gmail.com>
2018-05-28 13:56:08 +0200

    requetes sql

--- model/model.php
+++ model/model.php
@@ -2,3 +2,25 @@
 require_once '../utils/db.php';
 
 
+
+// insert a user
+function insertUser($sender_email,$receiver_email,$message){
+    global $db;
+    $response = $db->prepare("INSERT INTO user(sender_email, receiver_email, `message`) VALUES (':sender_email', ':receiver_email',':message')");
+    $response->bindParam(":sender_email", $sender_email);
+    $response->bindParam(":receiver_email", $receiver_email);
+    $response->bindParam(":message", $message);
+    $response->execute();
+    return $db->lastInsertedId();
+}
+
+//insert a file
+function insertFile($file_name, $file_size, $id_user){
+    global $db;
+    $response = $db->prepare("INSERT INTO `files`(`file_name`, `file_size`, `upload_date`, `delete_date`, id_user) VALUES ('file_name', ':file_size', CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 48 HOUR), ':id_user')");
+    $response->bindParam(":file_name",$file_name);
+    $response->bindParam(":file_size",$file_size);
+    $response->bindParam(":id_user",$id_user);
+    $response->execute();
+    
+}
