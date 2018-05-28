5148bb445a5681e98a6b1140ea293315fac51756
claireb25 <claire.bourgeois.cb@gmail.com>
2018-05-28 14:19:18 +0200

    3eme requete sql

--- model/model.php
+++ model/model.php
@@ -1,8 +1,6 @@
 <?
 require_once '../utils/db.php';
 
-
-
 // insert a user
 function insertUser($sender_email,$receiver_email,$message){
     global $db;
@@ -18,9 +16,18 @@ function insertUser($sender_email,$receiver_email,$message){
 function insertFile($file_name, $file_size, $id_user){
     global $db;
     $response = $db->prepare("INSERT INTO `files`(`file_name`, `file_size`, `upload_date`, `delete_date`, id_user) VALUES ('file_name', ':file_size', CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 48 HOUR), ':id_user')");
-    $response->bindParam(":file_name",$file_name);
-    $response->bindParam(":file_size",$file_size);
-    $response->bindParam(":id_user",$id_user);
+    $response->bindParam(":file_name",$file_name,PDO::PARAM_STR);
+    $response->bindParam(":file_size",$file_size, PDO::PARAM_INT);
+    $response->bindParam(":id_user",$id_user, PDO::PARAM_INT);
     $response->execute();
     
+};
+//get a file and all its infos
+function getFile(){
+    global $db;
+    $response = $db->prepare("SELECT sender_email,receiver_email,`message`,`file_name`,file_size,delete_date FROM user INNER JOIN files ON user.id = id_user WHERE user.id = :id_user");
+    $response->bindParam(":id_user", $id_user, PDO::PARAM_INT);
+    $response->execute();
+    return $response->fetch(PDO::FETCH_ASSOC);
 }
+
