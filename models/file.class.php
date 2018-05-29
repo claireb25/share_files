012fa335491d12de915b699cd3ac2dc67b2d7a05
claireb25 <claire.bourgeois.cb@gmail.com>
2018-05-29 08:48:46 +0200

    files

--- models/file.class.php
+++ models/file.class.php
@@ -1,8 +1,8 @@
 <?php 
 class File{
-    
+
     //insert a file
-    function insertFile($file_name, $file_size, $id_user){
+    public function insertFile($file_name, $file_size, $id_user){
     $db = new dbManager("share_files");
         $response = $db->getBdd()->prepare("INSERT INTO `files`(`file_name`, `file_size`, `upload_date`, `delete_date`, id_user) VALUES ('file_name', ':file_size', CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 48 HOUR), ':id_user')");
         $response->bindParam(":file_name",$file_name,PDO::PARAM_STR);
@@ -12,7 +12,7 @@ class File{
     }
 
     //get a file and all its infos
-    function getFile($id_user){
+    public function getFile($id_user){
         $db = new dbManager("share_files");
         $response = $db->getBdd()->prepare("SELECT sender_email,receiver_email,`message`,`file_name`,file_size,delete_date FROM user INNER JOIN files ON user.id = id_user WHERE user.id = :id_user");
         $response->bindParam(":id_user", $id_user, PDO::PARAM_INT);
@@ -21,7 +21,7 @@ class File{
     }
 
     // delete from bdd after 2 days
-    function deleteFile(){
+    public function deleteFile(){
         $this->_db = new dbManager();
         $response = $db->getBdd()->prepare('DELETE user, files FROM user INNER JOIN files ON user.id = files.id_user WHERE delete_date < NOW()');
         $response->execute();
