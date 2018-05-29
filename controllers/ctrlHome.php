f8200425e2588ebd40f6d88b9a8a26a5a24bb940
Julien-VIGNAUD <contact@julienvignaud.com>
2018-05-29 11:37:55 +0200

    mise a jour form

--- controllers/ctrlHome.php
+++ controllers/ctrlHome.php
@@ -12,5 +12,66 @@ $twig = new Twig_Environment($loader, array(
 $template = $twig->load('homepage.html.twig');
 echo $template->render(array('test'=>$test));
 
-$_POST[‘pseudo‘];
+
+// PHP Envoyeur Email //
+
+$_POST = ['senderEmail'];
+$senderEmail = $_POST['sender_email'];
+$point = strpos($senderEmail,".");
+$aroba = strpos($senderEmail,"@");
+
+if($point=='')
+{
+echo "Votre email doit comporter un <b>point</b>";
+}
+elseif($aroba=='')
+{
+echo "Votre email doit comporter un <b>'@'</b>";
+}
+else
+{
+echo "Votre email est: '<a href=\"mailto:"."$senderEmail"."\"><b>$senderEmail</b></a>'";
+} 
+
+// PHP Fin //
+
+// PHP Receveur Email //
+$receiverEmail = $_POST['receiver_email'];
+$point = strpos($receiverEmail,".");
+$aroba = strpos($receiverEmail,"@");
+
+if($point=='')
+{
+echo "Votre email doit comporter un <b>point</b>";
+}
+elseif($aroba=='')
+{
+echo "Votre email doit comporter un <b>'@'</b>";
+}
+else
+{
+echo "Votre email est: '<a href=\"mailto:"."$receiverEmail"."\"><b>$receiverEmail</b></a>'";
+} 
+
+// PHP Fin //
+
+// PHP Message //
+
+$message = $_POST['message'];
+
+if(empty($message))
+{
+exit();
+}
+
+// PHP Fin //
+
+if (isset($_POST['sender_email']) && empty($_POST['receiver_email']) && empty($_POST['']) && empty($_POST['file_name']))
+{
+    echo '<p>'.'hello '.$_POST['file_name'].'</p>';
+}
+else
+{
+    echo '<p>il manque un renseignement</p>';
+}
 
