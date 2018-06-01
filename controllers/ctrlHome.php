a4f5cef98ce3c2d2a54d12d5e430cf773f109e97
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-01 16:03:57 +0200

    mise en page email + ajout base

--- controllers/ctrlHome.php
+++ controllers/ctrlHome.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 require('models/file.class.php');
 
 
--- models/dbManager.php
+++ models/dbManager.php
@@ -5,8 +5,8 @@ class dbManager
     protected $db;
     private $host = "localhost";
 
-    private $login = "admin";
-    private $password = "online2017";
+    private $login = "claireb";
+    private $password = "7TQUD3FakR";
 
  
     public function __construct($database)
